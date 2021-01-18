ef85a74fbe402a09294c57c5b5f32a640a43d868
Jay <jaykamas@gmail.com>
2021-01-18 17:26:49 -0500

    adding resume as PDF

--- src/views/Resume.vue
+++ src/views/Resume.vue
@@ -1,5 +1,19 @@
 <template>
   <div class="resume">
     <h1>Resume goes here</h1>
+    <Resume />
+    
   </div>
 </template>
+
+<script>
+import Resume from "../assets/JayOliverResume.html"
+
+export default {
+  name: 'Resume-Container',
+  components: {
+    Resume
+  }
+}
+
+</script>
