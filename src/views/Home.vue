346ade1dabd8e0b78bc033eca82afb349bb4947f
Jay <jaykamas@gmail.com>
2021-01-18 12:59:24 -0500

    added linked in

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,7 +2,7 @@
   <div class="home">
     <HelloWorld msg="Hello I'm Jay Oliver"/>
     <h3>Github: https://github.com/Qwerter49</h3>
-    <h3>LinkedIn: </h3>
+    <h3>LinkedIn: https://www.linkedin.com/in/jay-oliver-29678b1b4/</h3>
   </div>
 </template>
 
